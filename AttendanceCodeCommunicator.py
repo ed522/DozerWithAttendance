6f2a157b21418eb1f65a809205ab2cb35837c469
ed522 <147557022+ed522@users.noreply.github.com>
2025-12-01 07:59:54 -0500

    update attendance communicator

--- AttendanceCodeCommunicator.py
+++ AttendanceCodeCommunicator.py
@@ -1,75 +1,99 @@
 import logging
 import json
+import socket
 import socketserver
+import threading
+from operator import truediv
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class AttendanceRequestHandler(socketserver.BaseRequestHandler):
-
-    CONST_CODE_VALID_DURATION = 30 # seconds
-
-    counter = 0
-
-    def handle(self):
-        connect_message = json.loads(self.request.recv(256))
-        if connect_message['type'] != 'connect':
-            logging.log(logging.ERROR, f"Failed to connect with client, wrong type {connect_message['type']}")
-            return # let client try again after 10s
-
-        response = {
-            'type': 'acknowledge',
-            'targeting': connect_message['type']
-        }
-        self.request.sendall(json.dumps(response))
-
-        while True:
-            message = json.loads(self.request.recv(256))
-
-            if message['type'] == 'heartbeat':
-                self.counter += 1
-                if self.counter != message['counter']:
-                    response = {
-                        'type': 'heartbeat_error',
-                        'counter': self.counter
-                    }
-                    self.request.sendall(json.dumps(response))
-
-                    logging.log(logging.WARN, f"Connection error with attendance code client: got counter {message['counter']}, expected {self.counter}")
-
-                else:
-                    response = {
-                        'type': 'acknowledge',
-                        'targeting': message['type'],
-                        'counter': self.counter
-                    }
-                    self.request.sendall(json.dumps(response))
-
-            elif message['type'] == 'code':
-                code = int(message['code'])
-                generation_time = int(message['generation_time'])
-                response = {
-                    'type': 'acknowledge',
-                    'targeting': message['type'],
-                    'code': code,
-                    'valid_to': generation_time + self.CONST_CODE_VALID_DURATION
-                }
-                self.request.sendall(json.dumps(response))
-                # TODO commit to a db somewhere
-
-            else:
-                logging.log(logging.WARN, f"Unknown message {message['type']}, ignoring")
-
 def run():
     # Specs:
-    # On connect, send JSON object with type = "connect"
+    #
+    # A screen sends out a 255.255.255.255 broadcast app: attendance, type: discovery, version: 1 (as of now), host matching local IP
+    # Server connects to host given in the parameter
+    #
+    # On connect, screen sends JSON object with type = "connect"
     # Server sends type = "acknowledge", targeting = "connect"
-    # On every code generation event, type = "code", code = <generated code>, generation_time = <generation Unix timestamp>
+    # On every code generation event (screen): type = "code", code = <generated code>, generation_time = <generation Unix timestamp>
     # Server acknowledges with type = "acknowledge", targeting = "code", code = <same>, valid_to = <expiry time>
-    # Every 10s, send type = "heartbeat", counter = <counter that increments each heartbeat>
+    # Every 10s, screen sends type = "heartbeat", counter = <counter that increments each heartbeat>
     # Server sends type = "acknowledge", targeting = "heartbeat", counter = <counter>
+    #
     # Errors:
     # If the heartbeat counters do not match, server sends type = "heartbeat_error", counter = <corrected value>
     # If connection fails, disconnect and try again after 10s
-    server = socketserver.TCPServer(('localhost', 5789), AttendanceRequestHandler)
-    server.serve_forever()
+    #
+    # Note that the "server" here is the discord bot, but the server is technically the tablet
+    def _communicate():
+        broadcast_in_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        broadcast_in_sock.connect(('0.0.0.0', 5789))
+
+    thread = threading.Thread(target=_communicate, daemon=True)
+    thread.start()
+
+def _discover(sock: socket.socket):
+
+    const_version = 1
+
+    while True:
+        message = json.loads(sock.recv) #something? TODO FIX
+        if message['app'] == 'attendance' and message['type'] == 'discovery' and message['version'] == const_version:
+            return message['host']
+
+def _communicate_after_handshake(request):
+
+    const_code_valid_duration = 30 # seconds
+
+    counter = 0
+
+    connect_message = json.loads(request.recv(256))
+    if connect_message['type'] != 'connect':
+        logging.log(logging.ERROR, f"Failed to connect with client, wrong type {connect_message['type']}")
+        return  # let client try again after 10s
+
+    response = {
+        'type': 'acknowledge',
+        'targeting': connect_message['type']
+    }
+    request.sendall(json.dumps(response))
+
+    while True:
+        message = json.loads(request.recv(256))
+
+        if message['type'] == 'heartbeat':
+            counter += 1
+            if counter != message['counter']:
+                response = {
+                    'type': 'heartbeat_error',
+                    'counter': counter
+                }
+                request.sendall(json.dumps(response))
+
+                logging.log(logging.WARN,
+                            f"Connection error with attendance code client: got counter {message['counter']}, expected {counter}")
+
+            else:
+                response = {
+                    'type': 'acknowledge',
+                    'targeting': message['type'],
+                    'counter': counter
+                }
+                request.sendall(json.dumps(response))
+
+        elif message['type'] == 'code':
+            code = int(message['code'])
+            generation_time = int(message['generation_time'])
+            response = {
+                'type': 'acknowledge',
+                'targeting': message['type'],
+                'code': code,
+                'valid_to': generation_time + const_code_valid_duration
+            }
+            request.sendall(json.dumps(response))
+            # TODO commit to a db somewhere
+
+        else:
+            logging.log(logging.WARN, f"Unknown message {message['type']}, ignoring")
